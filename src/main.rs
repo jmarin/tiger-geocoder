058070feefe43544fb8f90244a4327963fa82769
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-08-31 20:40:11 -0400

    Provide default PostgreSQL configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    dotenv().ok();
+    let db_url = env::var("POSTGRES_URL");
+    match db_url {
+        Ok(url) => println!("{:#?}", url),
+        Err(_) => println!("Please provide connection in POSTGRES_URL environment variable"),
+    }
 }
