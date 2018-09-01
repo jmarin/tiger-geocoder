c081343fcdb33dc122d66db824144daf065f96c2
Juan Marin Otero <juan.marin.otero@gmail.com>
2018-09-01 09:26:30 -0400

    Add parameter parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+#[macro_use]
+extern crate structopt;
 extern crate dotenv;
 
 use dotenv::dotenv;
 use std::env;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "address")]
+struct Opt {
+    address: String,
+}
 
 fn main() {
     dotenv().ok();
     let db_url = env::var("POSTGRES_URL");
     match db_url {
-        Ok(url) => println!("{:#?}", url),
+        Ok(url) => {
+            let opt = Opt::from_args();
+            let address = opt.address;
+            println!("{:#?}", address);
+        }
         Err(_) => println!("Please provide connection in POSTGRES_URL environment variable"),
     }
 }
